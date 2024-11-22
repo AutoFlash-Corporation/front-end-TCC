579f9b6a413b0adba2b3c0a04f66cc53d32911b3
LeticiaRDomingues <leticiardominguescontato@gmail.com>
2024-11-22 15:41:14 -0300

    Autenticação única usando withAuth.js - Autocard

--- src/pages/autocard.js
+++ src/pages/autocard.js
@@ -5,35 +5,16 @@ import Loading from "../component/Loading"; // Ajuste o caminho se necessário
 import Menu from "@/component/Menu";
 import SearchBar from "@/component/SearchBar";
 
+import withAuth from "../utils/withAuth"
+
 const AutoCardPage = () => {
-  const router = useRouter();
-  const [authChecked, setAuthChecked] = useState(false); // Apenas para verificar a autenticação
   const [searchResults, setSearchResults] = useState([]); // Armazenando resultados da pesquisa
 
   // Função para lidar com a pesquisa
   const handleSearch = (query) => {
     console.log("Pesquisando:", query);
-    // Aqui você pode fazer uma busca em um banco de dados, API, ou filtrar os dados
-    // Exemplo simples:
     setSearchResults([`Resultado para: ${query}`]); // Simulação de resultado
   };
-
-
-  useEffect(() => {
-    const accessToken = Cookies.get("access");
-
-    if (!accessToken) {
-      console.warn("Token de acesso ausente. Redirecionando para login...");
-      router.push("/login");
-    } else {
-      setAuthChecked(true); // Concluiu a verificação e está autenticado
-    }
-  }, [router]);
-
-  // Enquanto a verificação não foi concluída, exibe o Loading
-  if (!authChecked) {
-    return <Loading />;
-  }
 
   return (
     <div>
@@ -53,4 +34,4 @@ const AutoCardPage = () => {
   );
 };
 
-export default AutoCardPage;
+export default withAuth(AutoCardPage);
