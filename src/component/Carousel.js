84139d9449e97653478532e90ba448fb43d2ceef
LeticiaRDomingues <leticiardominguescontato@gmail.com>
2024-11-25 23:44:52 -0300

    Ajuste de tela em conjunto com o carrossel - Seção 3 - Landing Page

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -5,10 +5,8 @@ import useEmblaCarousel from 'embla-carousel-react';
 import styles from '../styles/carousel.module.css'; // Arquivo CSS
 
 const Carousel = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, speed: 5 });
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, speed: 4 });
 
-    const scrollPrev = () => emblaApi && emblaApi.scrollPrev();
-    const scrollNext = () => emblaApi && emblaApi.scrollNext();
 
     return (
         <div className={styles.embla} ref={emblaRef}>
@@ -16,9 +14,10 @@ const Carousel = () => {
                 <div className={styles.emblaSlide}>Slide 1</div>
                 <div className={styles.emblaSlide}>Slide 2</div>
                 <div className={styles.emblaSlide}>Slide 3</div>
+                <div className={styles.emblaSlide}>Slide 4</div>
+                <div className={styles.emblaSlide}>Slide 5</div>
+                <div className={styles.emblaSlide}>Slide 6</div>
             </div>
-            <button className={styles.prevButton} onClick={scrollPrev}>◀</button>
-            <button className={styles.nextButton} onClick={scrollNext}>▶</button>
         </div>
     );
 };
