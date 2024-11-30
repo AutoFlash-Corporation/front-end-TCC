c7d34d357685cc52b1e490fdafa51e2d008de4dd
LeticiaRDomingues <leticiardominguescontato@gmail.com>
2024-11-29 23:55:01 -0300

    Estruturação da tela de relatórios

--- src/pages/relatorios.js
+++ src/pages/relatorios.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import Menu from "@/component/Menu";
-import SearchBar from "@/component/SearchBar";
+import SearchBar from "../component/SearchBar"; // Importando a barra de pesquisa
+import style from "@/styles/home.module.css";
+import MyCalendar from "@/component/Calendar";
+import Logo from "@/component/Logo";
+import ShowBox from "@/component/ShowBox";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { IoPersonAdd } from "react-icons/io5";
+import { IoWallet } from "react-icons/io5";
+import { IoPaperPlaneSharp } from "react-icons/io5";
+
 import withAuth from "../utils/withAuth"
 
-const RelatoriosPage = () => {
+const Relatorios = ({ userName }) => {
   const [searchResults, setSearchResults] = useState([]); // Armazenando resultados da pesquisa
 
   // Função para lidar com a pesquisa
@@ -23,12 +32,46 @@ const RelatoriosPage = () => {
           <p key={index}>{result}</p>
         ))}
       </div>
-
       <div>
         <Menu />
+
+        <div className={style.Background}>
+          <main>
+          <ShowBox
+                icon={<FaPeopleGroup />}
+                title="Total de cards"
+                value="5 234"
+                description="Ver todos"
+                link="/card"
+            />
+            <ShowBox
+                icon={<IoPersonAdd />}
+                title="Cards revisados hoje"
+                value="150"
+                description="Ver todos"
+                link="/card"
+            />
+            <ShowBox
+                icon={<IoWallet />}
+                title="Previsão para amanhã"
+                value="98"
+                description="Ver Cards"
+                link="/card"
+            />
+            <ShowBox
+                icon={<IoPaperPlaneSharp />}
+                title="Total de conteúdos"
+                value="15"
+                description="Ver todos"
+                link="/conteudos"
+            />
+          </main>
+          <MyCalendar />
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
 
-export default withAuth(RelatoriosPage);
+export default withAuth(Relatorios);
